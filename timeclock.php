32684c984b04b201d10f421d32dc67875a57179c
Devin Walton <devinwalton@gmail.com>
2014-02-23 19:34:45 -0700

    Trying to connect.

--- timeclock.php
+++ timeclock.php
@@ -1,5 +1,5 @@
 <?php   
-         $mysqli = new mysqli("localhost", "phpuser", "password", "timeclock");
+         $mysqli = new mysqli("localhost", "root", "", "timeclock");
 
          if (mysqli_connect_errno()) {
             echo "Failed to connect to MySQL: " . $mysqli->connect_errno;
