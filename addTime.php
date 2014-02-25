d25f045e7401cd2e8e325ee07326e88b99a2e7c1
Devin Walton <devinwalton@gmail.com>
2014-02-25 01:25:47 -0700

    Adding to the sql

--- addTime.php
+++ addTime.php
@@ -3,14 +3,58 @@
 
          if (mysqli_connect_errno()) {
             echo "Failed to connect to MySQL: " . $mysqli->connect_errno;
-         }else
-         {
-            echo "Sucsess";
          }
 
          $data = file_get_contents("php://input");
-         echo $data;
+
          $objData = json_decode($data);
-         $hour =  $objData->hour;
-         echo "Hours Worked: ". $hour;
-          
+
+         $Hour =  $objData->hour;
+         $Min = $objData->min;
+         $StudentName = $objData->name;
+         $JobName = $objData->job;
+         $Task = $objData->task;
+
+
+         $JobExsists = $mysqli->query("SELECT JobID, JobName FROM Job");
+          $JobId = 0;
+          while($row = mysqli_fetch_array($JobExsists)){
+            if($row['JobName'] == $JobName)
+               $JobId = $row['JobId'];
+          }
+          if($JobId == 0) {
+            $string = "INSERT INTO Job
+               VALUES (NULL, $JobName)";
+          $mysqli->query($string);
+
+          $JobId = $mysqli->insert_id;
+          }
+          $string = "INSERT INTO Task
+               VALUES (NULL, $JobId,$Task)";
+          $mysqli->query($string);
+
+          $TaskId = $mysqli->insert_id;
+
+          $StudentExsists = $mysqli->query("SELECT StudentID, StudentName FROM Student");
+          $StudentId = 0;
+          while($row = mysqli_fetch_array($StudentExsists)){
+            if($row['StudentName'] == $StudentName)
+               $StudentId = $row['StudentId'];
+          }
+          if($StudentId == 0) {
+            $string = "INSERT INTO Student
+               VALUES (NULL, $StudentName)";
+          $mysqli->query($string);
+
+          $StudentId = $mysqli->insert_id;
+          }
+
+          $string = "INSERT INTO TimeWorked
+               VALUES (NULL, $TaskId,$StudentId,$Hour,$Min)";
+          $mysqli->query($string);
+
+
+          $string = "INSERT INTO StudentOnTask
+               VALUES (NULL,$StudentId,$TaskId,)";
+          $mysqli->query($string);
+           
