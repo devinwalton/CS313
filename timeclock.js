b3c4db7e106c15bf4947fc3d5e45a9d6ec07625f
Devin Walton <devinwalton@gmail.com>
2014-02-25 04:23:22 -0700

    gggg

--- timeclock.js
+++ timeclock.js
@@ -5,8 +5,8 @@ angular.module('timeclock', [])
   
     // The function that will be executed on button click (ng-click="search()")
     $scope.timeclock = function() {
-          $scope.values = [0,"0"];
-          $scope.labels = [0,"Everyone"];
+          $scope.values = ["0"];
+          $scope.labels = ["Everyone"];
          
         $http.post($scope.url, { "searchBy" : $scope.type}).
         success(function(data, status) {
@@ -14,9 +14,9 @@ angular.module('timeclock', [])
             for(var i = 0; i < data.length; i++)
             {
               if (i % 2 == 0)
-                $scope.values.add(data[i]);
+                $scope.values.push(data[i]);
               else
-                $scope.labels.add(data[i]);
+                $scope.labels.push(data[i]);
             }
           
             $scope.data = data;
