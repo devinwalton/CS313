c796c0994997eff59af13b5d5246546c6c6c1dee
Devin Walton <devinwalton@gmail.com>
2014-02-25 03:27:29 -0700

    Starting retrieval

--- timeclock.js
+++ timeclock.js
@@ -1,18 +1,12 @@
-
 angular.module('timeclock', [])
-    .controller('AddTimeCtrl', function($scope, $http) {
-    $scope.url = 'addTime.php'; // The url of our search
+    .controller('TimeclockCtrl', function($scope, $http) {
+    $scope.url = 'timeclock.php'; // The url of our search
          
-  $scope.hoursWorked = [0,1,2,3,4,5,6,7,8];
-  $scope.minsWorked = [0,15,30,45];
+  
     // The function that will be executed on button click (ng-click="search()")
-    $scope.addTime = function() {
+    $scope.timeclock = function() {
          
-        $http.post($scope.url, { "hour" : $scope.hour, 
-        							"min" : $scope.min,
-        							"name" : $scope.studentName,
-        	 						"job" : $scope.jobName, 
-        	 						"task" : $scope.task}).
+        $http.post($scope.url, { "searchBy" : $scope.type}).
         success(function(data, status) {
             $scope.status = "You have successfully logged your time.";
             $scope.data = data;
--- timeclock.php
+++ timeclock.php
@@ -3,91 +3,26 @@
 
          if (mysqli_connect_errno()) {
             echo "Failed to connect to MySQL: " . $mysqli->connect_errno;
-         }else
-         echo "Sucsess."
+         }
 
-         ?>
-<html lang = "en">
-<head>
-   <meta charset = "utf-8" />
-   <link rel="stylesheet" type="text/css" href="main.css"></link>  
-   <title>Boss Arena Manager</title>
-</head>
-<body>     
-   <!-- <header>
-      <ul class="nav-list">
-         <a id="nav-link" class="nav-active" 
-            href="index.html">
-            <li class="nav-link">home</li>
-         </a>
-      </ul>
-   </header>  -->
-   <div>         
-      <p class="head roundedBorder" align="center">Boss Arena Manager<p>
-   </div>
-   <div>
-    <img src="BossArenaB.png" width="200" />
-       
-      <div>
-         Search by :
-         <form name="register" method="post" action="signUp.php">
-         <select name="type">
-            <option value = "JOB" selected="selected">Job</option>
-            <option value = "STUDENT">Student</option>
-         </select>
-         <br />
-         Which one would you like to know about?
-         <select name="name">
-      </div>
-         
-      </select>
-      Total Hours Worked: 
-     
-    <img src="BossArenaG.png" width="200" align="right"/>
-   </div>
-</body>
-</html>
+         $data = file_get_contents("php://input");
 
-<?php
-          $Students = $mysqli->query("SELECT StudentID, StudentName FROM Student");
-          $Job = $mysqli->query("SELECT JobID, JobName FROM Job");
-         if($_GET["type"] == "JOB")
+         $objData = json_decode($data);
+         $type = objData->type
+
+          $Students = $mysqli->query("SELECT StudentId, StudentName FROM Student");
+          $Job = $mysqli->query("SELECT JobId, JobName FROM Job");
+         if($type == "JOB")
          {
             while ($row = $Job->fetch_assoc())
             {
-               echo "<option value = \"" . $row["JobID"] . "\">" . $row["JobName"] . "</option>\n";
+               echo "<option value = \"" . $row["JobId"] . "\">" . $row["JobName"] . "</option>\n";
             }
 
          }else
          {
             while ($row = $Student->fetch_assoc())
             {
-               echo "<option value = \"" . $row["StudentID"] . "\">" . $row["StudentName"] . "</option>\n";
+               echo "<option value = \"" . $row["StudentId"] . "\">" . $row["StudentName"] . "</option>\n";
             }
          }
-      ?>    
-
-       <?php
-         if($_GET["type"] == "JOB")
-         {
-            $queryString = "SELECT Hours, Minutes FROM TimeWorked "
-                . "WHERE JobID = $_GET["name"]";
-            $Time = $mysqli->query($queryString);
-         }else
-         {
-            $queryString = "SELECT Hours, Minutes FROM TimeWorked "
-                . "WHERE StudentID = $_GET["name"]";
-            $Time = $mysqli->query($queryString);
-         }
-
-         $hoursWorked;
-         $minutesWorked;
-         while ($row = $Time->fetch_assoc())
-         {
-            $hoursWorked += $row["Hours"];
-            $minutesWorked += $row["Minutes"];
-         }
-         $hoursWorked += $minutesWorked/60;
-
-         echo $hoursWorked . "hours";
-      ?>
