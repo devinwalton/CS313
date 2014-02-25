8cb1f1fc17432654fa781f3a9593e69ae6e8ebef
Devin Walton <devinwalton@gmail.com>
2014-02-24 23:56:10 -0700

    sdf

--- addTime.js
+++ addTime.js
@@ -1,4 +1,25 @@
 
+angular.module('timeclock', [])
+    .controller('AddTimeCtrl' function($scope, $http) {
+    $scope.url = 'addTime.php'; // The url of our search
+         
   $scope.hoursWorked = [0,1,2,3,4,5,6,7,8];
   $scope.minsWorked = [0,15,30,45];
   $scope.working = "hi";
+    // The function that will be executed on button click (ng-click="search()")
+    $scope.addTime = function() {
+         
+         $scope.request = [$scope.hours, $scope.mins];
+        $http.post($scope.url, { "data" : $scope.request}).
+        success(function(data, status) {
+            $scope.status = status;
+            $scope.data = data;
+            $scope.result = data; // Show result from server in our <pre></pre> element
+        })
+        .
+        error(function(data, status) {
+            $scope.data = data || "Request failed";
+            $scope.status = status;         
+        });
+    };
+});
